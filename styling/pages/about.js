74b7778600bd6981a71ce74a38eccc31d8fe2ef0
JagMax <gewoonmaks@gmail.com>
2022-10-30 14:37:38 +0300

    Styling

---
+++ styling/pages/about.js
@@ -0,0 +1,10 @@
+import styles from "../styles/About.module.css";
+
+export default function About() {
+  return (
+    <>
+      <button className="btn btn-primary">Primary</button>
+      <div className={styles.highlight}>About Page</div>
+    </>
+  );
+}
---
+++ styling/pages/contact.js
@@ -0,0 +1,5 @@
+import styles from "../styles/Contact.module.css";
+
+export default function Contact() {
+  return <div className={styles.hightlight}>Contact Page</div>;
+}
